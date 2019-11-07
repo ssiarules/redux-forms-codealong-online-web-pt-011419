5d894aac8375db945a578e665cd5aeab2813c650
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-07 03:08:19 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,8 @@
 export default function manageTodo(state = {
-  todos: [],
+  todos: [
+    { text: 'buy groceries'},
+    { text: 'watch netflix'}
+  ],
 }, action) {
 
   console.log("reducer received this action:", action);
